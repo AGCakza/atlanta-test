b345b261dcfd50785daf2592f2b3435f5526a9b1
Avtandil <avtandilk61@gmail.com>
2021-06-02 15:37:37 +0300

    logic fix

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import { InputField, PaymentCard } from './Components/Components'
 const App = () => {
   const [data, setData] = useState({
     bet: 0.088,
+    betMonth: 0.0073,
     creditSumm: 2000,
     monthPayment: 276,
     term: 1,
@@ -13,11 +14,11 @@ const App = () => {
   useEffect(() => console.log(data), [data])
   const onChange = e => {
     const x = e.target.value
-    if(e.target.name === 'objectPrice' && Number(e.target.value) <= 1000000) setData(prev => ({...prev, [e.target.name]: Number(e.target.value), monthPayment: (x - prev.firstPayment)*((prev.bet*Math.pow(1+prev.bet, prev.term*12))/(Math.pow(1+prev.bet, prev.term*12)-1)),
+    if(e.target.name === 'objectPrice' && Number(e.target.value) <= 1000000) setData(prev => ({...prev, [e.target.name]: Number(e.target.value), monthPayment: (x - prev.firstPayment)*((prev.betMonth*Math.pow(1+prev.betMonth, prev.term*12))/(Math.pow(1+prev.betMonth, prev.term*12)-1)),
       creditSumm: Math.round(x - (x/3)), firstPayment: Math.round(x/3)}))
-    if(e.target.name === 'firstPayment' && Number(e.target.value) <= data.objectPrice) setData(prev => ({...prev, [e.target.name]: Math.round(Number(e.target.value)), monthPayment: (prev.objectPrice - x)*((prev.bet*Math.pow(1+prev.bet, prev.term*12))/(Math.pow(1+prev.bet, prev.term*12)-1)),
+    if(e.target.name === 'firstPayment' && Number(e.target.value) <= data.objectPrice) setData(prev => ({...prev, [e.target.name]: Math.round(Number(e.target.value)), monthPayment: (prev.objectPrice - x)*((prev.betMonth*Math.pow(1+prev.betMonth, prev.term*12))/(Math.pow(1+prev.betMonth, prev.term*12)-1)),
       creditSumm: Math.round(prev.objectPrice - x)}))
-    if(e.target.name === 'term' && Number(e.target.value) <= 30 && Number(e.target.value) >= 1) setData(prev => ({...prev, [e.target.name]: Number(e.target.value), monthPayment: prev.creditSumm*((prev.bet*Math.pow(1+prev.bet, x*12))/(Math.pow(1+prev.bet, x*12)-1))}))
+    if(e.target.name === 'term' && Number(e.target.value) <= 30 && Number(e.target.value) >= 1) setData(prev => ({...prev, [e.target.name]: Number(e.target.value), monthPayment: prev.creditSumm*((prev.betMonth*Math.pow(1+prev.betMonth, x*12))/(Math.pow(1+prev.betMonth, x*12)-1))}))
   }
   return(
     <div className='container'>
